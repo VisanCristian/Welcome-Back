4215b43687cc53d9e68bc4ec40a0102d1a9b209f
vCristian <154425910+VisanCristian@users.noreply.github.com>
2025-11-07 13:48:33 +0200

    added Computer object

---
+++ Objects/Computer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Cristian Andrei Visan on 07.11.2025.
+//
+
+#include "Computer.h"
---
+++ Objects/Computer.h
@@ -0,0 +1,13 @@
+//
+// Created by Cristian Andrei Visan on 07.11.2025.
+//
+
+#ifndef OOP_COMPUTER_H
+#define OOP_COMPUTER_H
+
+
+class Computer {
+};
+
+
+#endif //OOP_COMPUTER_H
